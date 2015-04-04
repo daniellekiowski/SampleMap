c153e75cf2a24953d29cc7ae724e86e93aac1368
Danielle Kiowski <daniellekiowski@gmail.com>
2015-04-04 12:01:35 -0700

    tooltip

--- scripts/map.js
+++ scripts/map.js
@@ -17,11 +17,14 @@ function mapChart(selection) {
 
 			//make a hidden div that will be used to display data
 			var tip = d3.select(this).append("div")
-				.style("width", "100px")
-				.style("height", "50px")
+				.style("width","150px")
+				.style("height","50px")
+				.style("position","absolute")
 				.style("border-radius","15px")
 				.style("background-color", "#000")
 				.style("color","#fff")
+				.style("line-height","25px")
+				.style("text-align","center")
 				.style("opacity", 0);
 
 			d3.json("data/us-states.json",function(error,json){
@@ -44,7 +47,7 @@ function mapChart(selection) {
 				
 				function updateMap(data){
 
-					//
+					//join data to state definitions
 					for (var i = 0; i < data.length; i++) {
 						for (var j = 0; j < json.features.length; j++) {
 							if(data[i]["state"] == json.features[j]["properties"]["name"]){
@@ -52,12 +55,20 @@ function mapChart(selection) {
 							}
 						};
 					};
-					console.log(json.features);
+
 					svgMap.selectAll("path")
 						.data(json.features)
 						.attr("fill",function(e){return "#aaa";})
 						.on("mouseover",function(e){
-							console.log(e["properties"]["value"]?(e["properties"]["value"]+" "+units):"No Data");
+							tip.html(e["properties"]["name"]+"<br>"+(e["properties"]["value"]?(e["properties"]["value"]+" "+units):"No Data"))
+								.style("top",(d3.event.pageY+5)+"px")
+								.style("left",(d3.event.pageX+5)+"px")
+								.transition()
+								.style("opacity","1");
+						})
+						.on("mouseout",function(e){
+							tip.transition()
+								.style("opacity","0");
 						});
 				}
 			});
