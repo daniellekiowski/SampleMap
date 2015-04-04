b1f9c1a3a933403785522e1870d7311443a2b0db
Danielle Kiowski <daniellekiowski@gmail.com>
2015-04-03 18:28:52 -0700

    adding update map function and loading census data into drawn map

--- scripts/map.js
+++ scripts/map.js
@@ -4,20 +4,21 @@ function mapChart(selection) {
 	var title = "";
 	var width = 950;
 	var height = 500;
+	var units = "";
 
 	function map(selection){
 		selection.each(function(d,i){
-			d3.select(this)
-			var svgMap = d3.select(this).append('svg')
-				.attr('width',width)
-				.attr('height',height);
+			d3.select(this).append("h1").text(title);
+			var svgMap = d3.select(this).append("svg")
+				.attr("width",width)
+				.attr("height",height);
 
-			d3.json('data/us-states.json',function(error,json){
+			d3.json("data/us-states.json",function(error,json){
 				if(error){
 					console.log(error);
 					return;
 				}
-				
+				console.log(json.features);
 				//draw grey map
 				var geo = d3.geo.path(); 
 				svgMap.selectAll("path")
@@ -26,16 +27,28 @@ function mapChart(selection) {
 					.append("path")
 					.attr("d",geo)
 					.attr("fill","#aaa");
+
+				//fill in colors
+				updateMap(d);
+				
+				function updateMap(data){
+					for (var i = 0; i < data.length; i++) {
+						for (var j = 0; j < json.features.length; j++) {
+							if(data[i]["state"] == json.features[j]["properties"]["name"]){
+								json.features[j]["properties"]["value"] = data[i]["value"];
+							}
+						};
+					};
+					console.log(json.features);
+					svgMap.selectAll("path")
+						.data(json.features)
+						.attr("fill",function(e){return "#aaa";})
+						.on("mouseover",function(e){
+							console.log(e["properties"]["value"]+" "+units);
+						});
+				}
 			});
-
-			//fill in colors
-			updateMap(d);
-
 		});
-		
-		function updateMap(data){
-
-		}
 	}
 
 	map.title = function(_){
@@ -59,6 +72,14 @@ function mapChart(selection) {
 			return height;
 		}
 		height = _;
+		return map;
+	}
+
+	map.units = function(_){
+		if(!arguments.length){
+			return units;
+		}
+		units = _;
 		return map;
 	}
 
---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+d3.csv('../data/transportation.csv',function(error,data){
+	
+	if(error){
+		console.log(error);
+		return;
+	}
+
+	//Constructing array of data to use when building map. Start at index 1 because the object at index 0 contains headers.
+	var mapData = [];
+	for (var i = 1; i < data.length; i++) {
+		mapData.push({
+			state: data[i]["GEO.display-label"],
+			value: data[i]["HC01_EST_VC118"]
+		});
+	};
+
+	var map = new mapChart().title("Average Commute Time by State").units("min");
+	d3.select("body").datum(mapData).call(map);
+})
